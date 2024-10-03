33e3f71f648cbaf5d3a409ba380e40ef12173a3e
Aashishson <theaashishsoni@gmail.com>
2024-10-03 14:45:31 +0530

    Tic-Tac-Toe_Game

--- Tic-Tac-Toe_Game.cpp
+++ Tic-Tac-Toe_Game.cpp
@@ -5,26 +5,38 @@ char currentmarker;
 char currentplayer;
 char player1;
 char player2;
+
+// Function to initialize the board
+void initializeBoard() {
+    char num = '1';
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            board[i][j] = num++;
+        }
+    }
+}
+
 //to draw the board or display it
 void displayboard(){
-    cout << "   |   |   " << endl;
-    cout << " " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << endl;
-    cout << "___|___|___" << endl;
-    cout << "   |   |   " << endl;
-    cout << " " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << endl;
-    cout << "___|___|___" << endl;
-    cout << "   |   |   " << endl;
-    cout << " " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl;
-    cout << "   |   |   " << endl;
+    std::cout << "   |   |   " << endl;
+    std::cout << " " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << endl;
+    std::cout << "___|___|___" << endl;
+    std::cout << "   |   |   " << endl;
+    std::cout << " " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << endl;
+    std::cout << "___|___|___" << endl;
+    std::cout << "   |   |   " << endl;
+    std::cout << " " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl;
+    std::cout << "   |   |   " << endl;
         }
+
  //to place the mark in the tic-tac-toe grid
 bool placemark(){
     int choice;
-    cout<<"enter your choice between (1 to 9):";
-    cin>>choice;
+    std::cout<<"enter your choice between (1 to 9):";
+    std::cin>>choice;
 
-    if(cin.fail()) { // Check if the input is valid
-        cin.clear(); // Clear the error 
+    if(std::cin.fail()) { // Check if the input is valid
+        std::cin.clear(); // Clear the error 
     }
     int rows = (choice - 1)/3;
     int columns = (choice -1)%3;
@@ -33,11 +45,11 @@ bool placemark(){
             board[rows][columns] = currentmarker;
             return true;
         } else {
-            cout << "Slot already taken, choose another number!" << endl;
+            std::cout << "Slot already taken, choose another number!" << endl;
             return false;
         }
     } else {
-        cout << "Invalid number!" << endl;
+        std::cout << "Invalid number!" << endl;
         return false;
     }
 }
@@ -80,40 +92,57 @@ char checkwinner(){
 }
 
 int main(){
-cout<<"Tic-Tac-Toe game board"<<endl;
-displayboard();
-cout<<"player 1 choose your marker between 'X' or 'O' "<<endl;
-cin>>currentmarker;
-if(currentmarker == 'X'){
-   player1 = 'X';
-   player2 = 'O';
-}else{
-    player1 = 'O';
-    player2 = 'X';
-}
-char currentplayer = player1;
-int winner =0;
-int turns  =0;
+    char playagain;
+    char currentplayer = player1;
+   
+do {
 
-//Main game loop
-while(winner ==0 && turns<9){
-cout << currentmarker <<"'s turn"<<endl;
+    initializeBoard();
+    std::cout<<"Tic-Tac-Toe game board"<<endl;
+    displayboard();
+    std::cout<<"player 1 choose your marker between 'X' or 'O' "<<endl;
+    std::cin>>currentmarker;
+    if(currentmarker == 'X'){
+      player1 = 'X';
+      player2 = 'O'; 
+      }
+    else{
+      player1 = 'O';
+      player2 = 'X';
+      }
+       int winner =0;
+       int turns  =0;
+    
+   
+    
 
-if(!placemark()){
+ //Main game loop
+  while(winner ==0 && turns<9){
+   std::cout << currentmarker <<"'s turn"<<endl;
+
+    if(!placemark()){
     continue;
-}
-displayboard();
-winner =checkwinner();
+    }
+      displayboard();
+      winner =checkwinner();
+      if(winner==0){
+        swapping();
+      }
+      turns++;
+    }
+ //to check the result
+ if(winner ==0 && turns ==9){
+    std::cout<<"it's a tie "<<endl;
+ }else{
+    std::cout<<"player"<<(winner==player1? "1":"2")<< "wins!"<<endl;
+ }
+ // Ask if players want to play again
+        cout << "Do you want to play again? (Y/N): ";
+        std::cin >> playagain;
+} while (playagain == 'Y' || playagain == 'y');
 
-    swapping();
-
-turns++;
-}
-//to check the result
-if(winner ==0 && turns ==9){
-    cout<<"it's a tie "<<endl;
-}else{
-    cout<<"player"<<(winner == 'X'? "1": "2")<< "wins!"<<endl;
-}
+        
     return 0;
-}
+    } 
+
+
